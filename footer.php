3a781ad70e60d278d3070beb860eee6d4215910d
RaThida <100819767+RaThida@users.noreply.github.com>
2022-10-07 16:24:04 +0700

    partials file

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!-- Footer Section Starts -->
+<link rel="stylesheet" href="../css/index.css">
+<div class="footer text-center">
+       <div class="wrapper">
+            <p>2022 All right reserved, Foot House. Developed By -<a href="">IT TEAM</a></p>
+        </div>
+       </div>
+       <!-- Footer Section Ends -->
+
+    </body>
+</html>
